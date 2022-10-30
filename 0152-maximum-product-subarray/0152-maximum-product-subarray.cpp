714243ad7c1d88d29ff91182e82cff9a65784110
Rishi Neelkanth <71087332+RishiNeelkanth@users.noreply.github.com>
2022-10-31 02:04:38 +0530

    Time: 8 ms (85.79%), Space: 13.8 MB (71.44%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,8 +5,16 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++)
         {
-        l=(l? l : 1) * nums[i];
-            r=(r ? r : 1) * nums[n-1-i];
+         if(l)
+         {
+             l*=nums[i];
+         }
+            else{ l=1*nums[i];}
+            if(r)
+            {
+                r*=nums[n-1-i];
+            }
+            else{ r=1*nums[n-1-i];}
             maxi=max(maxi,max(l,r));
         }
     return maxi;
