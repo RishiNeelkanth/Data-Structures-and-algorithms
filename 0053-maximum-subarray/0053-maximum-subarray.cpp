640fd8e86a62935906d77619c27d8b44ebf5471b
Rishi Neelkanth <71087332+RishiNeelkanth@users.noreply.github.com>
2022-10-31 23:30:04 +0530

    Time: 336 ms (19.38%), Space: 67.8 MB (11.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curMax = 0, maxTillNow = INT_MIN;
-        for(auto c : nums)
-            curMax = max(c, curMax + c),
-            maxTillNow = max(maxTillNow, curMax);
-        return maxTillNow;
+        int sum=0,maxi=INT_MIN;
+        for(auto it : nums)
+        {
+            sum+=it;
+            maxi=max(maxi,sum);
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return maxi;
     }
 };
+
+//class Solution {
+//public:
+    //int maxSubArray(vector<int>& nums) {
+        //int curMax = 0, maxTillNow = INT_MIN;
+        //for(auto c : nums)
+           // curMax = max(c, curMax + c),
+            //maxTillNow = max(maxTillNow, curMax);
+        //return maxTillNow;
+    //}
+//};
